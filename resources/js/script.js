ba69d91d864623025d0c2841bd367648772fbde8
ratulislam <iratul010@gmail.com>
2022-09-04 10:39:08 +0600

    blog-site add

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,21 @@
 "use strict";
-
+const modalBody = document.getElementById("modal-body");
 let navLink = document.querySelector(".nav-link");
 let noDataFound = document.querySelector(".no-data-found");
 let fetchTwo, dataTwo;
 const newsBlog = document.getElementById("news-blog");
 const newsUl = document.getElementById("news-ul");
+const countNewsBlog = document.getElementById("count-news-blog");
 const toggleSpinner = (hasSpinner) => {
-  const loader = document.getElementById("loader");
+  let load = document.getElementById("loader");
   if (hasSpinner) {
-    loader.classList.remove("d-none");
+    load.classList.remove("d-none");
   } else {
-    loader.classList.add("d-none");
+    load.classList.add("d-none");
   }
 };
-const reporter = async function (id) {
+toggleSpinner(false);
+const reporter = async function () {
   const categoryUrl = ` https://openapi.programming-hero.com/api/news/categories`;
 
   const res = await fetch(categoryUrl);
@@ -24,21 +26,25 @@ const reporter = async function (id) {
     const newsUrl = `  https://openapi.programming-hero.com/api/news/category/${id}`;
     const newsRes = await fetch(newsUrl);
     const newsData = await newsRes.json();
-    console.log(newsData);
+    // console.log(newsData);
     dataTwo = newsData.data;
+
     ///news Blog child Element
 
     //end
     dataTwo = dataTwo.slice(0, 10);
+
     let length = dataTwo.length;
+    countNewsBlog.innerHTML = `${length} items found for category Entertainment`;
     if (length === 0) {
+      toggleSpinner(false);
       noDataFound.classList.remove("d-none");
       newsBlog.appendChild(noDataFound);
     } else {
       ////news-div
 
       noDataFound.classList.add("d-none");
-
+      let id = 0;
       dataTwo.forEach((el) => {
         let classAdd = function (className) {
           newsChildDiv.classList.add(`${className}`);
@@ -50,12 +56,12 @@ const reporter = async function (id) {
         classAdd("p-4");
         classAdd("rounded");
         classAdd("item");
+
         //all data ---create
-        console.log(el);
-        // console.log(el.thumbnail_url);
+
         let authorName = el.author.name;
         let authorimg = el.author.img;
-        console.log(authorimg);
+        let newsDetails = el.details.slice(0, 500);
         let img = el.image_url;
         let authorHeadLine = el.title;
         let ratingNumber = el.rating.number;
@@ -63,6 +69,7 @@ const reporter = async function (id) {
         let time = new Date(parseInt(timeZone) / (1000 * 60 * 60 * 24));
         let year = time.getFullYear();
         let days = time.getDay();
+        // console.log(el);
         const monthNames = [
           "January",
           "February",
@@ -90,10 +97,7 @@ const reporter = async function (id) {
           <div class="col mb-5">
             <h2 class="fs-1 p-3">${authorHeadLine}</h2>
             <p class="fs-3 p-3">
-              From our favourite UK influencers to the best missives from Milan and the coolest New Yorkers, read on some
-              of the best fashion blogs out there, and for even more inspiration, do head to our separate black fashion
-              influencer . Fancy some shopping deals? Check out these amazing sales: Zara Black Friday, ASOS Black Friday,
-              Missoma Black Friday and Gucci Black Friday.
+              ${newsDetails}...
             </p>
           </div>
           <div class="col details-logo">
@@ -135,8 +139,8 @@ const reporter = async function (id) {
                   </span>
                 </div>
               </div>
-              <div class="item-4">
-                <a class="btn fs-2">
+              <div class="item-4" id="${id}">
+                <a class="btn fs-2 " type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                   <i class="fa-solid fa-arrow-right"> </i>
                 </a>
               </div>
@@ -146,8 +150,57 @@ const reporter = async function (id) {
       </div>
         
         `;
-
+        id += 1;
+        toggleSpinner(false);
         newsBlog.appendChild(newsChildDiv);
+        // modal
+        let reporterName, reporterImg, reporterDate, reporterTotalView, reporterRating, reporterBadge;
+        [newsChildDiv].forEach((data) => {
+          data.addEventListener("click", function (e) {
+            e.preventDefault();
+            const clicked = e.target.closest(".item-4");
+
+            // Guard clause
+            if (clicked) {
+              if (!clicked) return;
+              console.log(e.target);
+              const item = e.target.closest(".item-4");
+              let id = item.getAttribute("id");
+              console.log(dataTwo[id]);
+              reporterName = dataTwo[id].author.name;
+              reporterImg = dataTwo[id].author.img;
+              reporterDate = new Date(dataTwo[id].author.published_date);
+              reporterRating = dataTwo[id].rating.number;
+              reporterBadge = dataTwo[id].rating.badge;
+              reporterTotalView = dataTwo[id].total_view;
+
+              console.log(reporterDate.getMonth());
+
+              const div = document.createElement("div");
+              modalBody.innerHTML = "";
+              div.innerHTML = `
+              <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-8 col-8" style="height: 200px; width: 220px">
+                  <img style="width: 100%" src="${reporterImg}" alt="" />
+                </div>
+                <div class="col-md-4  ms-auto text-center  ">
+                  <h3 class="heading-primary fs-2">${reporterName}</h3>
+                  <p class="fs-4">${
+                    monthNames[reporterDate.getMonth()]
+                  } ${reporterDate.getDate()}, ${reporterDate.getFullYear()}</p>
+                  <p class="fs-4">Rating: ${reporterRating}, <span class="fw-semibold">${reporterBadge}</span> </p>
+                  <p class="fs-4">Total View: <span class="fw-semibold">${reporterTotalView}</span> </p>
+                
+                </div>
+              </div>
+            </div>`;
+
+              modalBody.appendChild(div);
+              // (reporterName = 0), (reporterImg = 0), (reporterDate = 0);
+            }
+          });
+        });
       });
     }
   };
@@ -158,8 +211,6 @@ const reporter = async function (id) {
     newsLi.classList.add("nav-item");
     let id = el.category_id;
 
-    // newsLi.setAttribute("id", `${id}`);
-    // navLink.setAttribute("id", `${id}`);
     newsLi.innerHTML = `
         
         <a class="nav-link" href="${id}"> ${el.category_name}</a>
@@ -170,13 +221,12 @@ const reporter = async function (id) {
 
   newsUl.addEventListener("click", function (e) {
     e.preventDefault();
-
+    toggleSpinner(true);
     newsBlog.innerHTML = "";
     if (e.target.classList.contains("nav-link")) {
       const id = e.target.getAttribute("href");
-
+      console.log(id);
       fetchTwo(id);
-      toggleSpinner(false);
     }
   });
 };
